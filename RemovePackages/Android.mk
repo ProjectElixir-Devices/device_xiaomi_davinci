d5b99bbaec0c9860f452beeabcc2e04e2b6aadf9
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    davinci: Remove unwanted GApps Packages
    Co-Authored-By: zαι∂ кнαη <kzaid0997@gmail.com>

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,50 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    AmbientStreaming \
+    arcore \
+    Camera2 \
+    CarrierLocation \
+    CarrierMetrics \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DiagnosticsToolPrebuilt \
+    DMService \
+    Drive \
+    Gallery2 \
+    GCS \
+    GoogleCamera \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PlayAutoInstallConfig \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    USCCDM \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTubeMusicPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
