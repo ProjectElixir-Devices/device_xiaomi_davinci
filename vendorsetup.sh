ece230c94ac111153f156630c4265477e85deac6
AMWolfstein <76236780+AMWolfstein@users.noreply.github.com>
2023-01-21 02:52:28 +0200

    davinci: Bringup for Project elixir

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+# nuke
+rm -rf vendor/xiaomi
+rm -rf kernel/xiaomi
+rm -rf hardware/xiaomi
+rm -rf hardware/lineage/compat
+
+# clone
+git clone -b 13 --depth=1 https://github.com/ProjectElixir-Devices/proprietary_vendor_xiaomi_davinci.git vendor/xiaomi/davinci
+git clone -b 14 --depth=1 https://github.com/xiaomi-davinci/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b lineage-20 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-20.0 --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
+
+rm -rf hardware/xiaomi/megvii
