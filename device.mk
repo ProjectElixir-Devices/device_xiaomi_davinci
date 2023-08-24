d2c9bde544d6d202f7e4473b7aa366ba9be4f185
AMWolfstein <ahmedhero4040@gmail.com>
2023-08-24 11:32:23 +0300

    davinci: Convert overlays into PRO
    Co-Authored-By: shaughzam <6667815+shaughzam@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -340,9 +340,15 @@ $(foreach sku, CN GLOBAL, \
         frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_$(sku)/com.nxp.mifare.xml))
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    ApertureOverlay \
+    CarrierConfigOverlay \
+    DavinciSettingsProviderOverlay \
+    FrameworksResDavinci \
+    SettingsResDavinci \
+    SystemUIResDavinci \
+    TelephonyResCommon \
+    WifiOverlay
 
 PRODUCT_PACKAGES += \
     DavinciCNSettingsProviderOverlay \
@@ -387,7 +393,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
-    CarrierConfigOverlay \
     libjson \
     libril \
     librilutils \
@@ -495,7 +500,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml \
     libwpa_client \
-    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
