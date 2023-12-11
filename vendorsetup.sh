5d00d19e7ed5e04f99ef48717c60b7ff3d103076
AMWolfstein <ahmedhero4040@gmail.com>
2023-12-11 11:52:31 +0200

    davinci: Let's build with Leica miuicamera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,5 +11,6 @@ git clone -b 14 --depth=1 https://github.com/xiaomi-davinci/kernel_xiaomi_sm6150
 git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 git clone -b lineage-21.0 --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
 git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
+git clone -b leica-5.0  https://bitbucket.org/itzdfplayer/vendor_davinci-miuicamera.git vendor/xiaomi/miuicamera
 
 rm -rf hardware/xiaomi/megvii
