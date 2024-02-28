c0015c81dd5a8bb4dcd7f0224dcc865c751a7b00
S A U R A V <dwarmachine24@gmail.com>
2024-02-28 23:00:07 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,14 +20,6 @@ git clone --depth=1 https://github.com/projectelixir-davinci/android_kernel_xiao
 rm -rf prebuilts/clang/host/linux-x86/r498229b
 git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
 
-# Hardware Lineage Compat
-rm -rf hardware/lineage/compat
-git clone --depth=1 -b lineage-21.0 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
-
-# Hardware Xiaomi
-rm -rf hardware/xiaomi
-git clone https://github.com/Project-Elixir/hardware_xiaomi hardware/xiaomi
-
 # Vibrator
 rm -rf vendor/qcom/opensource/vibrator
 git clone --depth=1 https://github.com/PixelExperience/vendor_qcom_opensource_vibrator vendor/qcom/opensource/vibrator
